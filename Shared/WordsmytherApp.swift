6c8767c8a591db37197cc3229d1f9bd9b4f354f8
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2021-12-29 12:47:57 +0100

    Initial Commit

---
+++ Shared/WordsmytherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsmytherApp.swift
+//  Shared
+//
+//  Created by Kevin Romero Peces-Barba on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WordsmytherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
